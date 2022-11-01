dabd424cd62a8d532108f22d1d70a111dfd859a6
ridwanrahmn <ridwanrahmansyah2425@gmail.com>
2022-11-01 10:47:09 +0700

    commit StatefulActivity

--- app/src/main/java/com/example/android_app_01/StatefulActivity.kt
+++ app/src/main/java/com/example/android_app_01/StatefulActivity.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-class StaticActivity : ComponentActivity () {
+class StatefulActivity : ComponentActivity () {
 
 }
 
