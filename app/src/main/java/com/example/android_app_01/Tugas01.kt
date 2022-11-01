f47b407b1920dcfadfc23c743c2285a765a2c303
ridwanrahmn <ridwanrahmansyah2425@gmail.com>
2022-11-01 09:53:40 +0700

    commit Tugas01

--- app/src/main/java/com/example/android_app_01/Tugas01.kt
+++ app/src/main/java/com/example/android_app_01/Tugas01.kt
@@ -38,20 +38,21 @@ fun codes(){
             Surface(modifier = Modifier
                 .weight(1f)
                 .fillMaxHeight(),
-                color = Color(242, 209, 119)){
-                contents()
+                color = Color(146, 105, 100)){
+                contents1()
+
             }
             Surface(modifier = Modifier
                 .weight(1f)
                 .fillMaxHeight(),
                 color = Color(237, 120, 104)) {
-                contents()
+                contents2()
             }
             Surface(modifier = Modifier
                 .weight(1f)
                 .fillMaxHeight(),
                 color = Color(196, 104, 184)) {
-                contents()
+                contents3()
             }
         }
         Row(modifier = Modifier.fillMaxHeight()){
@@ -59,26 +60,26 @@ fun codes(){
                 .weight(1f)
                 .fillMaxHeight(),
                 color = Color(104, 136, 217)){
-                contents()
+                contents4()
             }
             Surface(modifier = Modifier
                 .weight(1f)
                 .fillMaxHeight(),
                 color = Color(102, 212, 113)) {
-                contents()
+                contents5()
             }
             Surface(modifier = Modifier
                 .weight(1f)
                 .fillMaxHeight(),
                 color = Color(130, 237, 235)) {
-                contents()
+                contents6()
             }
         }
     }
 }
 
 @Composable
-fun contents(){
+fun contents1(){
     Column (
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
@@ -98,13 +99,183 @@ fun contents(){
             modifier = Modifier.height(10.dp)
         )
         Text(
+            text = "@ridwanrahmn_",
+            color = Color.Black,
+            fontSize = 13.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "1",
+            color = Color.Black,
+            fontSize = 10.sp,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+@Composable
+fun contents2(){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .width(100.dp)
+            .height(100.dp)
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.logo_with_bg),
+            contentDescription = "Logo",
+            alignment = Alignment.Center,
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(87.dp, 87.dp)
+        )
+        Spacer(
+            modifier = Modifier.height(10.dp)
+        )
+        Text(
+            text = "Ridwan",
+            color = Color.Black,
+            fontSize = 13.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "2",
+            color = Color.Black,
+            fontSize = 10.sp,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+@Composable
+fun contents3(){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .width(100.dp)
+            .height(100.dp)
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.logo_with_bg),
+            contentDescription = "Logo",
+            alignment = Alignment.Center,
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(87.dp, 87.dp)
+        )
+        Spacer(
+            modifier = Modifier.height(10.dp)
+        )
+        Text(
             text = "ridwanrahmn",
             color = Color.Black,
             fontSize = 13.sp,
             textAlign = TextAlign.Center
         )
         Text(
-            text = "191344027",
+            text = "3",
+            color = Color.Black,
+            fontSize = 10.sp,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+@Composable
+fun contents4(){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .width(100.dp)
+            .height(100.dp)
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.logo_with_bg),
+            contentDescription = "Logo",
+            alignment = Alignment.Center,
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(87.dp, 87.dp)
+        )
+        Spacer(
+            modifier = Modifier.height(10.dp)
+        )
+        Text(
+            text = "ridwanrahmn",
+            color = Color.Black,
+            fontSize = 13.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "4",
+            color = Color.Black,
+            fontSize = 10.sp,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+@Composable
+fun contents5(){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .width(100.dp)
+            .height(100.dp)
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.logo_with_bg),
+            contentDescription = "Logo",
+            alignment = Alignment.Center,
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(87.dp, 87.dp)
+        )
+        Spacer(
+            modifier = Modifier.height(10.dp)
+        )
+        Text(
+            text = "Rahmansyah",
+            color = Color.Black,
+            fontSize = 13.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "5",
+            color = Color.Black,
+            fontSize = 10.sp,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+@Composable
+fun contents6(){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .width(100.dp)
+            .height(100.dp)
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.logo_with_bg),
+            contentDescription = "Logo",
+            alignment = Alignment.Center,
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(87.dp, 87.dp)
+        )
+        Spacer(
+            modifier = Modifier.height(10.dp)
+        )
+        Text(
+            text = "@ridwanrahmn_",
+            color = Color.Black,
+            fontSize = 13.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "6",
             color = Color.Black,
             fontSize = 10.sp,
             textAlign = TextAlign.Center
